ccc9391380fc57c252d3044f5ed883a01d2fc94d
ryudongjae <rdj10149@gmail.com>
2021-04-03 23:47:25 +0900

    Initial commit

---
+++ src/main/java/ryudongjae/noticeBoard/NoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package ryudongjae.noticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryudongjae/noticeBoard/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ryudongjae.noticeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
